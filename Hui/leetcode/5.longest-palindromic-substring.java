fce06940b6a1139713cec94dd9273696fc2e01bc
huiliclara <lihuiclara@163.com>
2021-01-17 11:49:16 -0800

    longest palindromic subtring O(N) solution

--- Hui/leetcode/5.longest-palindromic-substring.java
+++ Hui/leetcode/5.longest-palindromic-substring.java
@@ -8,6 +8,42 @@
 class Solution {
     public String longestPalindrome(String s) {
         
+        char[] c = new char[2 * s.length() + 3];
+        c[0] = '$'; 
+        c[c.length - 1] = '^';
+        for(int i = 1; i < c.length - 1; i++) {
+            if ((i - 2) % 2 == 0) {
+                c[i] = s.charAt((i - 2) / 2);
+            } else {
+                c[i] = '#';
+            }
+        }
+
+        int retIndex = -1, maxLen = -1;
+        int curMaxIndex = 0, curMaxLen = 0;
+
+        int[] dp = new int[c.length - 1];
+        for(int i = 1; i < c.length - 1; i++) {
+            if (i < curMaxIndex) {
+                dp[i] = Math.min(dp[2 * curMaxIndex - i], curMaxLen - i);
+            }
+            while(c[i - dp[i] - 1] == c[i + dp[i] + 1]) {
+                dp[i]++;
+            }
+            if(i + dp[i] > curMaxLen) {
+                curMaxIndex = i;
+                curMaxLen = i + dp[i];
+            }
+            if(dp[i] > maxLen) {
+                retIndex = i;
+                maxLen = dp[i];
+            }
+        }
+
+        return s.substring((retIndex - maxLen) / 2, (retIndex - maxLen) / 2 + maxLen);
+
+        /*
+        O(N^2) Solution
         if (s == "") return "";
         boolean[] dp = new boolean[s.length()];
         int start = 0, end = 0;
@@ -32,6 +68,7 @@ class Solution {
 
         return s.substring(start, end + 1);
 
+        */
         /* Another solution
         O(n^2)
 
